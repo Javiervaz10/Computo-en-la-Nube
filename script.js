27825aa117d302b4c0050907caf64cad6f8c3b21
Francisco Javier <105751133+Javiervaz10@users.noreply.github.com>
2024-04-10 11:54:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hola Mundo");
+// Contenido del archivo JavaScript
+window.onload = function() {
+    var mensajeElemento = document.getElementById("mensaje");
+    var mensaje = "Hola Mundo";
+    mensajeElemento.innerHTML = mensaje;
+};
